b8e7e846e9b423c809f0cb09ce37368faf592e6d
Dontgoingforyou <mishael000@yandex.ru>
2025-01-13 13:12:13 +0300

    cw8 create

--- config/settings.py
+++ config/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework_simplejwt',
     'celery',
+    'django_celery_beat',
 
     'habits',
     'users',
@@ -69,7 +70,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": os.getenv('NAME'),
-        "USER": 'postgres',
+        "USER": os.getenv('POSTGRES_USER'),
         "PASSWORD": os.getenv('PASSWORD'),
         "HOST": os.getenv('HOST'),
         "PORT": os.getenv('PORT'),
@@ -113,7 +114,7 @@ CELERY_TIMEZONE = TIME_ZONE
 CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
 
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
 
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
--- habits/tasks.py
+++ habits/tasks.py
@@ -1,6 +1,5 @@
 from celery import shared_task
-from telegram import Bot
-from django.conf import settings
+from config.telegram_bot import bot
 
 from habits.models import Habit
 
@@ -8,6 +7,12 @@ from habits.models import Habit
 @shared_task
 def send_telegram_reminder(habit_id):
     habit = Habit.objects.get(id=habit_id)
-    bot = Bot(token=settings.TELEGRAM_BOT_TOKEN)
     message = f'Напоминание о привычке {habit.action} в {habit.place} в {habit.time}.'
     bot.send_message(chat_id=habit.user.profile.telegram_id, text=message)
+
+
+@shared_task
+def schedule_reminders():
+    habits = Habit.objects.filter(is_active=True)
+    for habit in habits:
+        send_telegram_reminder.delay(habit.id)
